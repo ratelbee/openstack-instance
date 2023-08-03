8cb8547aab09244e3e19aea2e532ed13bf024458
k.stepanov <kyustepanov@basistech.ru>
2023-08-04 00:20:45 +0300

    Revert "Init Commit"
    This reverts commit 365a097c327f1e90d6f3d6ddb96a831d1027f06d.

--- main.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    openstack = {
-      source = "terraform-provider-openstack/openstack"
-      version = ">=1.51.1"
-    }
-  }
-  required_version = ">= 1.1.19"
-}
